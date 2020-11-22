ff4c63cfb9b06739bd2db84f82c447032c9abbc1
long.wang <long.wang@joowing.com>
2020-11-22 23:55:32 +0800

    表达式代码提交

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 function MyController($scope, $timeout) {
   var updatedClock = function() {
+    // JavaScript自身的特点 以及它在传递值和引用时的不同处理 通常在是视图中通过对象的属性
     $scope.clock = { 
       now: new Date()
     }
--- js/my-controller.js
+++ js/my-controller.js
@@ -1,5 +1,62 @@
-angular.module('myApp', [])
-  .controller('myController', function($rootScope, $scope) {
+angular.module('emailParser', [])
+  .config(['$interpolateProvider', function($interpolateProvider) {
+    $interpolateProvider.startSymbol('__')
+    $interpolateProvider.endSymbol('__')
+  }])
+
+  .factory('EmailParser',['$interpolate', function($interpolate) {
+    // 处理解析的服务
+    return {
+      parse: function(text, context) {
+        var template = $interpolate(text)
+        return template(context)
+      }
+    }
+  }])
+var app = angular.module('myApp', ['emailParser'])
+
+  app.controller('myController', ['$rootScope', '$scope', '$parse', '$interpolate', 'EmailParser',
+  function($rootScope, $scope, $parse, $interpolate, EmailParser) {
     $scope.name = 'long'
-    console.log($rootScope)
+    $scope.$watch('expr', function(newVal, oldVal, scope) {
+      if (newVal != oldVal) {
+        // 用该表达式来设置parseFun
+        var parseFun = $parse(newVal)
+        $scope.parsedValue = parseFun(scope)
+      }
+    })
+
+    $scope.$watch('emailBody', function(body) {
+      if (body) {
+        //var template = $interpolate(body);
+        $scope.previewText = EmailParser.parse(body, {
+          to: $scope.to
+        })
+      }
+    })
+  }])
+
+  
+
+  app.controller('firstController', function($scope) {
+    
+    $scope.counter = 1
+    
+    $scope.add = function add(value) {
+      $scope.counter += value
+    }
+
+    $scope.subtract = function subtract(value) {
+      $scope.counter -= value
+    }
+  })
+
+  app.controller('ParentController', function($scope) {
+    $scope.person = { greeted: false }
+  })
+
+  app.controller('ChildController', function($scope) {
+    $scope.sayHello = function() {
+      $scope.person.name = 'long'
+    }
   })
