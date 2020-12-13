58ed5c8d717ec9cec42f5f9efcb0afe5954b61fb
long.wang <long.wang@joowing.com>
2020-12-13 20:52:28 +0800

    filter commit

--- js/app.js
+++ js/app.js
@@ -1,4 +1,8 @@
 function MyController($scope, $timeout) {
+  // 首字母是否为大写
+  $scope.isCapitalized = function(str) {
+    return str[0] == str[0].toUpperCase()
+  }
   var updatedClock = function() {
     // JavaScript自身的特点 以及它在传递值和引用时的不同处理 通常在是视图中通过对象的属性
     $scope.clock = { 
