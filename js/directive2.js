ee3c170edc8d28ab002384c088c50a9105306619
wanglong <18392700041@163.com>
2021-01-03 18:02:36 +0800

    directive

--- js/directive2.js
+++ js/directive2.js
@@ -6,14 +6,33 @@ angular.module('myApp', [])
   .directive('myDirective', function() {
     return {
       restrict: 'EA',
-      require: '?ngModel',
-      template: '<h3>{{myController.msg}}<h3>',
+      require: 'ngModel',
       controllerAs: 'myController',
       controller: function() {
         this.msg = 'hello word'
       },
-      link($scope, $elem, attrs, ctrl) {
-        console.log(ctrl)
+      link($scope, $elem, attrs, ngModel) {
+        console.log(ngModel)
+        ngModel.$render = function() {
+          $elem.html(ngModel.$viewValue || 'None')
+        }
       }
     }
   })
+
+  // .directive('myDirective', function() {
+  //   return {
+  //     compile: function(tEle, tAttrs, transcludeFn) {
+  //       var tplEl = angular.element('<div><h2></h2></div>')
+  //       var h2 = tplEl.controller.find('h2')
+  //       h2.attr('type', tAttrs.type)
+  //       h2.attr('ng-model', tAttrs.ngModel)
+  //       h2.val('hello')
+  //       tEle.replaceWith(tplEl)
+  //       return function(scope, ele, attrs) {
+
+  //       }
+
+  //     }
+  //   }
+  // })
